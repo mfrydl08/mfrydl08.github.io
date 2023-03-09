2ffc9d9f9048182048f082ff66df0c9c8edc8607
mfrydl08 <frydlm@yahoo.com>
2023-03-09 15:43:50 -0500

    All teams version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,4 @@
-// Roadkill version
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {RosterComponent} from "./roster/roster.component";
-import {ScheduleComponent} from "./schedule/schedule.component";
-import {HomeComponent} from "./home/home.component";
-import {ResultsComponent} from "./results/results.component";
-import {StandingsComponent} from "./standings/standings.component";
-import {TeamDetailComponent} from "./team-detail/team-detail.component";
-
-const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: "full" },
-  {path: 'home', component: HomeComponent},
-  {path: 'roster', component: RosterComponent},
-  {path: 'schedule', component: ScheduleComponent},
-  {path: 'results', component: ResultsComponent},
-  {path: 'standings', component: StandingsComponent},
-  {path: 'detail/:id', component: TeamDetailComponent},
-  { path: '**', redirectTo: '/home', pathMatch: 'full' },
-];
-
-@NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-
-
-// // All teams version
+// // Roadkill version
 // import {NgModule} from "@angular/core";
 // import {RouterModule, Routes} from "@angular/router";
 // import {RosterComponent} from "./roster/roster.component";
@@ -40,7 +11,7 @@ export class AppRoutingModule { }
 // const routes: Routes = [
 //   {path: '', redirectTo: '/home', pathMatch: "full" },
 //   {path: 'home', component: HomeComponent},
-//   {path: 'roster/:id', component: RosterComponent},
+//   {path: 'roster', component: RosterComponent},
 //   {path: 'schedule', component: ScheduleComponent},
 //   {path: 'results', component: ResultsComponent},
 //   {path: 'standings', component: StandingsComponent},
@@ -54,3 +25,32 @@ export class AppRoutingModule { }
 //   exports: [RouterModule]
 // })
 // export class AppRoutingModule { }
+
+
+// All teams version
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {RosterComponent} from "./roster/roster.component";
+import {ScheduleComponent} from "./schedule/schedule.component";
+import {HomeComponent} from "./home/home.component";
+import {ResultsComponent} from "./results/results.component";
+import {StandingsComponent} from "./standings/standings.component";
+import {TeamDetailComponent} from "./team-detail/team-detail.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: "full" },
+  {path: 'home', component: HomeComponent},
+  {path: 'roster/:id', component: RosterComponent},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: 'results', component: ResultsComponent},
+  {path: 'standings', component: StandingsComponent},
+  {path: 'detail/:id', component: TeamDetailComponent},
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,67 +1,10 @@
-//Roadkill Version
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {Player} from "../models/player";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatSort} from "@angular/material/sort";
-import {ActivatedRoute} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
-import {StandingsService} from "../standings/standings.service";
-
-@Component({
-  selector: 'app-roster',
-  templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
-})
-export class RosterComponent implements OnInit {
-  @Input() teamName!: string;
-
-  public players: Array<Player> = [];
-  public dataSource = new MatTableDataSource();
-
-  initColumns = [
-    { name: 'fullName', display: 'Name' },
-    { name: 'position', display: 'Position' },
-    { name: 'number', display: 'Jersey Number' },
-  ];
-
-  public displayedColumns = this.initColumns.map((col) => col.name);
-
-  @ViewChild(MatSort, { static: true }) sort!: MatSort
-
-    constructor(private route: ActivatedRoute,
-                private httpClient: HttpClient,
-                public standingsService: StandingsService) {
-  }
-
-  public ngOnInit() {
-    this.httpClient.get("assets/rosterData.json").subscribe(data =>{
-      const players = <Player[]>data;
-      const filteredData = players.filter((player) => player.teamName === "Roadkill");
-      this.dataSource.data = filteredData;
-    });
-
-    this.dataSource.sort = this.sort;
-  }
-
-  public applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-}
-
-// //All Teams Version
+// //Roadkill Version
 // import {Component, Input, OnInit, ViewChild} from '@angular/core';
 // import {Player} from "../models/player";
 // import {MatTableDataSource} from "@angular/material/table";
 // import {MatSort} from "@angular/material/sort";
-// import {HttpClient} from "@angular/common/http";
-// import {Location} from "@angular/common";
 // import {ActivatedRoute} from "@angular/router";
+// import {HttpClient} from "@angular/common/http";
 // import {StandingsService} from "../standings/standings.service";
 //
 // @Component({
@@ -85,26 +28,19 @@ export class RosterComponent implements OnInit {
 //
 //   @ViewChild(MatSort, { static: true }) sort!: MatSort
 //
-//   constructor(private location: Location,
-//               private route: ActivatedRoute,
-//               private httpClient: HttpClient,
-//               public standingsService: StandingsService) {
+//     constructor(private route: ActivatedRoute,
+//                 private httpClient: HttpClient,
+//                 public standingsService: StandingsService) {
 //   }
 //
 //   public ngOnInit() {
-//     const id = parseInt(<string>this.route.snapshot.paramMap.get('id'), 10);
-//
-//     this.teamName = this.standingsService.getTeamNameById(id);
-//
 //     this.httpClient.get("assets/rosterData.json").subscribe(data =>{
 //       const players = <Player[]>data;
-//       const filteredData = players.filter((player) => player.teamName === this.teamName);
-//
+//       const filteredData = players.filter((player) => player.teamName === "Roadkill");
 //       this.dataSource.data = filteredData;
 //     });
 //
 //     this.dataSource.sort = this.sort;
-//     this.dataSource.filter = this.teamName.trim().toLowerCase();
 //   }
 //
 //   public applyFilter(event: Event) {
@@ -112,18 +48,82 @@ export class RosterComponent implements OnInit {
 //
 //     this.dataSource.filter = filterValue.trim().toLowerCase();
 //
-//     if (filterValue.trim() === '' || filterValue.trim() === null) {
-//       this.dataSource.filter = this.teamName.trim().toLowerCase();
-//     } else {
-//       this.dataSource.filter = filterValue.trim().toLowerCase();
-//     }
-//
 //     if (this.dataSource.paginator) {
 //       this.dataSource.paginator.firstPage();
 //     }
 //   }
-//
-//   public goBack(): void {
-//     this.location.back();
-//   }
 // }
+
+//All Teams Version
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Player} from "../models/player";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
+import {HttpClient} from "@angular/common/http";
+import {Location} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
+import {StandingsService} from "../standings/standings.service";
+
+@Component({
+  selector: 'app-roster',
+  templateUrl: './roster.component.html',
+  styleUrls: ['./roster.component.css']
+})
+export class RosterComponent implements OnInit {
+  @Input() teamName!: string;
+
+  public players: Array<Player> = [];
+  public dataSource = new MatTableDataSource();
+
+  initColumns = [
+    { name: 'fullName', display: 'Name' },
+    { name: 'position', display: 'Position' },
+    { name: 'number', display: 'Jersey Number' },
+  ];
+
+  public displayedColumns = this.initColumns.map((col) => col.name);
+
+  @ViewChild(MatSort, { static: true }) sort!: MatSort
+
+  constructor(private location: Location,
+              private route: ActivatedRoute,
+              private httpClient: HttpClient,
+              public standingsService: StandingsService) {
+  }
+
+  public ngOnInit() {
+    const id = parseInt(<string>this.route.snapshot.paramMap.get('id'), 10);
+
+    this.teamName = this.standingsService.getTeamNameById(id);
+
+    this.httpClient.get("assets/rosterData.json").subscribe(data =>{
+      const players = <Player[]>data;
+      const filteredData = players.filter((player) => player.teamName === this.teamName);
+
+      this.dataSource.data = filteredData;
+    });
+
+    this.dataSource.sort = this.sort;
+    this.dataSource.filter = this.teamName.trim().toLowerCase();
+  }
+
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (filterValue.trim() === '' || filterValue.trim() === null) {
+      this.dataSource.filter = this.teamName.trim().toLowerCase();
+    } else {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  public goBack(): void {
+    this.location.back();
+  }
+}
