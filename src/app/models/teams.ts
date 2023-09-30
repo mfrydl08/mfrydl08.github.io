3acb34169a933b2f23e2d3facf884b31ae473f45
mfrydl08 <frydlm@yahoo.com>
2023-09-30 10:54:09 -0400

    Updated schedule for Fall 2023

--- src/app/models/teams.ts
+++ src/app/models/teams.ts
@@ -1,4 +1,5 @@
 export class Teams {
   public id = 0;
+  public division = 0;
   public teamName = "";
 }
--- src/app/standings/standings.service.ts
+++ src/app/standings/standings.service.ts
@@ -28,43 +28,44 @@ export class StandingsService {
     switch (this.appService.selectedSessionValue) {
       case "0":
         this.teamList.push(
-          {id: 1, teamName:"Absinthe Minded"},
-          {id: 2, teamName:"Agony of Da Feet"},
-          {id: 3, teamName:"Blue City"},
-          {id: 4, teamName:"Internationals"},
-          {id: 5, teamName:"Jolly Green Giants"},
-          {id: 6, teamName:"KMP"},
-          {id: 7, teamName:"Mustangs FC"},
-          {id: 8, teamName:"Roadkill"},
-          {id: 9, teamName:"Rusty Knights"}
+          {id: 1, division: 1, teamName:"Absinthe Minded"},
+          {id: 2, division: 1, teamName:"Agony of Da Feet"},
+          {id: 3, division: 1, teamName:"Blue City"},
+          {id: 4, division: 1, teamName:"Internationals"},
+          {id: 5, division: 1, teamName:"Jolly Green Giants"},
+          {id: 6, division: 1, teamName:"KMP"},
+          {id: 7, division: 1, teamName:"Mustangs FC"},
+          {id: 8, division: 1, teamName:"Roadkill"},
+          {id: 9, division: 1, teamName:"Rusty Knights"}
         );
         break;
       case '1':
         this.teamList.push(
-          {id: 1, teamName:"Absinthe Minded"},
-          {id: 2, teamName:"Agony of Da Feet"},
-          {id: 3, teamName:"Blue City"},
-          {id: 4, teamName:"Internationals"},
-          {id: 5, teamName:"Jolly Green Giants"},
-          {id: 6, teamName:"KMP"},
-          {id: 7, teamName:"Mustangs FC"},
-          {id: 8, teamName:"Roadkill"},
-          {id: 9, teamName:"Rusty Knights"},
-          {id: 10, teamName: "FC Frederick Aces"}
+          {id: 1, division: 1, teamName:"Absinthe Minded"},
+          {id: 2, division: 1, teamName:"Agony of Da Feet"},
+          {id: 3, division: 1, teamName:"Blue City"},
+          {id: 4, division: 1, teamName:"Internationals"},
+          {id: 5, division: 1, teamName:"Jolly Green Giants"},
+          {id: 6, division: 1, teamName:"KMP"},
+          {id: 7, division: 1, teamName:"Mustangs FC"},
+          {id: 8, division: 1, teamName:"Roadkill"},
+          {id: 9, division: 1, teamName:"Rusty Knights"},
+          {id: 10, division: 1, teamName: "FC Frederick Aces"}
         );
         break;
       case '2':
         this.teamList.push(
-          {id: 1, teamName:"Absinthe Minded"},
-          {id: 2, teamName:"Agony of Da Feet"},
-          {id: 3, teamName:"Blue City"},
-          {id: 4, teamName:"Internationals"},
-          {id: 5, teamName:"Jolly Green Giants"},
-          {id: 6, teamName:"KMP"},
-          {id: 7, teamName:"Mustangs FC"},
-          {id: 8, teamName:"Roadkill"},
-          {id: 9, teamName:"Rusty Knights"},
-          {id: 10, teamName: "FC Frederick Aces"}
+          {id: 1, division: 1, teamName:"Absinthe Minded"},
+          {id: 2, division: 1, teamName:"Agony of Da Feet"},
+          {id: 3, division: 1, teamName:"Blue City"},
+          {id: 4, division: 1, teamName:"Mustang"},
+          {id: 5, division: 2, teamName:"Internationals"},
+          {id: 6, division: 2, teamName:"Jolly Green Giants"},
+          {id: 7, division: 2, teamName:"KMP"},
+          {id: 8, division: 2, teamName:"Roadkill"},
+          {id: 9, division: 2, teamName:"Rusty Knights"},
+          {id: 10, division: 2, teamName: "FC Frederick Aces"},
+          {id: 11, division: 2, teamName: "Point of Rocks"}
         );
         break;
 
