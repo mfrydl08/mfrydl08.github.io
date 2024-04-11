fff87128d0d655734fe75e69119e8005bebc0028
mfrydl08 <frydlm@yahoo.com>
2024-04-11 16:10:54 -0400

    Updated data for Spring 2024

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,7 @@ export class AppService {
     { id : '2', sessionName: 'Fall 2023'},
     { id : '3', sessionName: 'Spring 2024'},
     { id : '4', sessionName: 'Summer 2024'},
-    { id : '5', sessionName: 'Fall 2023'}
+    { id : '5', sessionName: 'Fall 2024'}
   ];
 
   public session = this.sessions[2].sessionName;
